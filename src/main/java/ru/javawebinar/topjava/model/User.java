bcde310581dd361f5e6e73333763b0104bc966e5
vit73123 <vit73123@gmail.com>
2023-06-21 11:36:04 +0300

    L02 HWC fixC: User

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -1,9 +1,12 @@
 package ru.javawebinar.topjava.model;
 
 import org.springframework.util.CollectionUtils;
+import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.util.*;
+
+import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class User extends AbstractNamedEntity {
 
@@ -20,7 +23,7 @@ public class User extends AbstractNamedEntity {
     private int caloriesPerDay;
 
     public User(Integer id, String name, String email, String password, Role... roles) {
-        this(id, name, email, password, SecurityUtil.authUserCaloriesPerDay(), true, Arrays.asList(roles));
+        this(id, name, email, password, DEFAULT_CALORIES_PER_DAY, true, Arrays.asList(roles));
     }
 
     public User(Integer id, String name, String email, String password, int caloriesPerDay, boolean enabled, Collection<Role> roles) {
