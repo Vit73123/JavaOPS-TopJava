fefa9e12ce8f346559d93f8f6a6e1ea30a650e61
vit73123 <vit73123@gmail.com>
2023-07-02 15:43:20 +0300

    L04 HWC Opt: @Rule MealServiceTest

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,8 +1,14 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Stopwatch;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
@@ -14,6 +20,7 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.MealTestData.*;
@@ -26,8 +33,39 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@Ignore
+//@Ignore
 public class MealServiceTest {
+    private static final Logger log = LoggerFactory.getLogger(MealServiceTest.class);
+
+    private static final StringBuilder builder = new StringBuilder();
+
+    @Rule
+    public final Stopwatch stopwatch = new Stopwatch() {
+        @Override
+        protected void finished(long nanos, Description description) {
+            builder
+                    .append(String.format("%-30s", description.getMethodName()))
+                    .append(TimeUnit.NANOSECONDS.toMillis(nanos))
+                    .append(" ms\n");
+        }
+    };
+
+    @Rule
+    public TestWatcher testWatcher = new TestWatcher() {
+        @Override
+        protected void starting(Description description) {
+            log.info(description.getMethodName());
+        }
+    };
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("==================================================");
+        System.out.println("Test results: MealServiceTest (duration)");
+        System.out.println("--------------------------------------------------");
+        System.out.print(builder);
+        System.out.println("==================================================");
+    }
 
     @Autowired
     private MealService service;
