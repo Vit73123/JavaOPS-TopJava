95352ad24f26b86eca16ada55551670157048e66
vit73123 <vit73123@gmail.com>
2023-07-14 08:36:33 +0300

    L05 HWC fixC2: DataJpaMealRepository fix @Transactional

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -31,7 +31,6 @@ public class DataJpaMealRepository implements MealRepository {
     }
 
     @Override
-    @Transactional
     public boolean delete(int id, int userId) {
         return crudRepository.delete(id, userId) != 0;
     }
