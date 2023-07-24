37b823981f19aa0e6835e149b597e0d95546b6e9
vit73123 <vit73123@gmail.com>
2023-07-24 14:53:57 +0300

    L06 HWC Opt: 2.2 Add Role 'USER' to Admin

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -20,9 +20,12 @@ import java.util.*;
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+//@QueryHints(value = { @QueryHint(name = "hibernate.query.passDistinctThrough", value = "false")})
+//@QueryHint(name = "hibernate.query.passDistinctThrough", value = "false")
 @NamedQueries({
         @NamedQuery(name = User.DELETE, query = "DELETE FROM User u WHERE u.id=:id"),
-        @NamedQuery(name = User.BY_EMAIL, query = "SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email=?1"),
+        @NamedQuery(name = User.BY_EMAIL, query = "SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email=?1",
+                hints = @QueryHint(name = "hibernate.query.passDistinctThrough", value = "false")),
         @NamedQuery(name = User.ALL_SORTED, query = "SELECT u FROM User u ORDER BY u.name, u.email"),
 })
 @Entity
