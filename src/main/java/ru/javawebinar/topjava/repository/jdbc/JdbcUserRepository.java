09b6045e87b6d220b267f58ef40e68536b59ceb2
vit73123 <vit73123@gmail.com>
2023-07-30 08:42:29 +0300

    L06 HWC fixC1: JdbcUserRepository

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -1,9 +1,10 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.support.DataAccessUtils;
-import org.springframework.jdbc.core.*;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -15,7 +16,6 @@ import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.util.ValidationUtil;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
@@ -23,13 +23,39 @@ import java.util.*;
 @Transactional(readOnly = true)
 public class JdbcUserRepository implements UserRepository {
 
-    private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
-
     private final JdbcTemplate jdbcTemplate;
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     private final SimpleJdbcInsert insertUser;
+
+    private static final ResultSetExtractor<List<User>> userExtractor = rs -> {
+        List<User> users = new ArrayList<>();
+        Map<Integer, User> usersMap = new HashMap<>();
+        while (rs.next()) {
+            int userId = rs.getInt("id");
+            User user = usersMap.get(userId);
+            if (user == null) {
+                user = new User(
+                        userId,
+                        rs.getString("name"),
+                        rs.getString("email"),
+                        rs.getString("password"),
+                        rs.getInt("calories_per_day"),
+                        rs.getBoolean("enabled"),
+                        rs.getDate("registered"),
+                        EnumSet.noneOf(Role.class)
+                );
+                usersMap.put(userId, user);
+                users.add(user);
+            }
+            String role = rs.getString("role");
+            if (role != null) {
+                user.getRoles().add(Role.valueOf(role));
+            }
+        }
+        return users;
+    };
 
     @Autowired
     public JdbcUserRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
@@ -88,52 +114,20 @@ public class JdbcUserRepository implements UserRepository {
     public User get(int id) {
         return DataAccessUtils.singleResult(jdbcTemplate.query(
                 "SELECT * FROM users u LEFT JOIN user_role r ON u.id = r.user_id WHERE u.id=?",
-                new UserExtractor(), id));
+                userExtractor, id));
     }
 
     @Override
     public User getByEmail(String email) {
         return DataAccessUtils.singleResult(jdbcTemplate.query(
                 "SELECT * FROM users u LEFT JOIN user_role r ON u.id = r.user_id WHERE u.email=?",
-                new UserExtractor(), email));
+                userExtractor, email));
     }
 
     @Override
     public List<User> getAll() {
         return jdbcTemplate.query(
                 "SELECT * FROM users u LEFT JOIN user_role r ON u.id = r.user_id ORDER BY u.name, u.email",
-                new UserExtractor());
-    }
-
-    private static class UserExtractor implements ResultSetExtractor<List<User>> {
-
-        @Override
-        public List<User> extractData(ResultSet rs) throws SQLException, DataAccessException {
-            List<User> users = new ArrayList<>();
-            Map<Integer, User> usersMap = new HashMap<>();
-            while (rs.next()) {
-                int userId = rs.getInt("id");
-                User user = usersMap.get(userId);
-                if (user == null) {
-                    user = new User(
-                            userId,
-                            rs.getString("name"),
-                            rs.getString("email"),
-                            rs.getString("password"),
-                            rs.getInt("calories_per_day"),
-                            rs.getBoolean("enabled"),
-                            rs.getDate("registered"),
-                            new HashSet<>()
-                    );
-                    usersMap.put(userId, user);
-                    users.add(user);
-                }
-                String role = rs.getString("role");
-                if (role != null) {
-                    user.getRoles().add(Role.valueOf(role));
-                }
-            }
-            return users;
-        }
+                userExtractor);
     }
 }
