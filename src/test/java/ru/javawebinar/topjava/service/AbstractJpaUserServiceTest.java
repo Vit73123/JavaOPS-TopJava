bc1515f3204e3cc69d27f4ed8cb6d2ff7f4b2f73
vit73123 <vit73123@gmail.com>
2023-07-31 00:27:17 +0300

    L06 HWC fixC1: Opt2 Spring cache disable in profiles

--- src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
@@ -1,9 +1,4 @@
 package ru.javawebinar.topjava.service;
 
-import org.springframework.test.context.ContextConfiguration;
-
-@ContextConfiguration(
-        "classpath:spring/spring-cacheOverride.xml"
-)
 public abstract class AbstractJpaUserServiceTest extends AbstractUserServiceTest {
 }
--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -20,7 +20,8 @@ import static ru.javawebinar.topjava.util.ValidationUtil.getRootCause;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
+        "classpath:spring/spring-db.xml",
+        "classpath:spring/spring-cacheOverride.xml"
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
