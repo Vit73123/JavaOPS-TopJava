653eaab43f91968a3d73f64e0d36f017a08c7c0b
vit73123 <vit73123@gmail.com>
2023-08-01 09:06:58 +0300

    L06 HWC fixC2: DataJpaUserServiceTest

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -3,10 +3,9 @@ package ru.javawebinar.topjava.service.datajpa;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
-import ru.javawebinar.topjava.service.AbstractJpaUserServiceTest;
 import ru.javawebinar.topjava.MealTestData;
-import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.AbstractJpaUserServiceTest;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import static ru.javawebinar.topjava.MealTestData.MEAL_MATCHER;
@@ -20,7 +19,6 @@ public class DataJpaUserServiceTest extends AbstractJpaUserServiceTest {
         User user = service.getWithMeals(ADMIN_ID);
         USER_MATCHER.assertMatch(user, admin);
         MEAL_MATCHER.assertMatch(user.getMeals(), MealTestData.meals);
-        System.out.println();
     }
 
     @Test
